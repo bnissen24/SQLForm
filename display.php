c8584f045058ecb0e90906bcbff46e6742068fef
Bryan Nissen <bryannissen@gmail.com>
2019-11-12 14:39:44 -0500

    first commit

---
+++ display.php
@@ -0,0 +1,12 @@
+<?php
+$firstName = filter_input(INPUT_POST, 'firstName');
+$lastName = filter_input(INPUT_POST, 'lastName');
+$birthday = filter_input(INPUT_POST, 'birthday');
+$email = filter_input(INPUT_POST, 'email');
+$password = filter_input(INPUT_POST, 'password');
+
+echo "First Name: $firstName <br>";
+echo "Last Name: $lastName <br>";
+echo "Birthday: $birthday <br>";
+echo "Email: $email <br>";
+echo "Password: $password <br>";
